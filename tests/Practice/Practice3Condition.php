097f3ed10a69e3c88dc55ff616c0d79d8db26d45
robin <robin.graffin@gmail.com>
2024-01-12 14:42:25 +0100

    exo 3 finit en fonction des commentaires sous les exos

--- tests/Practice/Practice3Condition.php
+++ tests/Practice/Practice3Condition.php
@@ -10,20 +10,15 @@ class Practice3Condition
     {
         $condition = count($a) === 3;
 
-        // le triple egal sert a rien, le == suffit, sount renvera toujours un int y'a pas d'ambiguité possible qui merite le ===
-    
         return $condition;
     }
 
     // use ctype_upper pour vérifier qu'il y'a des majuscules dans un élément
-    // $condition doit tester si $a contient exclusivement des majuscules 
+    // $condition doit tester si $a contient exclusivement des majuscules
     // la condition doit retourner false
     public static function exercice2($a)
     {
-        // $condition = !in_array(false, array_map('ctype_upper', $a));
-
-        // là ton in_array test si il y a un element dans l'array "a" qui n'est pas une majuscule. l'exo c'est tester si tout les element de "a" sont des majuscules.
-        // tes pas loin mais c'est pas exactement ce qui est demandé
+        $condition = !in_array(true, array_map('ctype_upper', $a));
 
         return $condition;
     }
@@ -33,10 +28,7 @@ class Practice3Condition
     // la condition doit retourner false
     public static function exercice3($a)
     {
-        // $condition = !in_array(false, array_map('is_string', $a));
-
-        // pareil la ton in_array test si y'a un element qui n'est pas une string dans l'array, c'est pas ce qu'on demande
-        // tes pas loin mais c'est pas exactement ce qui est demandé
+        $condition = !in_array(true, array_map('is_string', $a));
 
         return $condition;
     }
@@ -54,9 +46,7 @@ class Practice3Condition
     // la condition doit retourner true
     public static function exercice5($a, $b)
     {
-        $condition = ($a === $b[1]);
-
-        // tes parentaise servent a rien
+        $condition = $a === $b[1];
 
         return $condition;
     }
@@ -74,8 +64,8 @@ class Practice3Condition
     // la condition doit retourner true
     public static function exercice7($a, $b)
     {
-        // $condition = empty(array_diff($b, [$a]));
-        // $condition = !$condition;
+        $condition = empty(array_diff($b, [$a]));
+        $condition = !$condition;
 
         // non la tu test la différence entre le contenu de a et b c'est pas ce que demande
 
@@ -95,11 +85,7 @@ class Practice3Condition
     // la condition doit retourner true
     public static function exercice9($a, $b)
     {
-
-        $condition1 = array_reverse($b);
-        $condition = ($a == $condition1);
-
-        // en une ligne stp ta "condition1" sert a rien
+        $condition = ($a == array_reverse($b));
 
         return $condition;
     }
@@ -108,11 +94,8 @@ class Practice3Condition
     // la condition doit retourner true
     public static function exercice10($a, $b)
     {
+        $condition = (array_map('strtoupper', $a) == $b);
 
-        $condition1 = array_map('strtoupper', $a);
-        $condition = ($condition1 == $b);
-
-        // en une ligne stp ta "condition1" sert a rien
 
         return $condition;
     }
